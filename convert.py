e3a5bbc5f468d61147e896d4e26faa466f4cb882
vstkl <dev@vstkl.xyz>
2024-11-05 17:41:44 +0100

    added hidden

--- convert.py
+++ convert.py
@@ -1,5 +1,10 @@
 import re
 import markdown
+from xhtml2pdf import pisa
+
+# Import the BytesIO class from the io module
+from io import BytesIO
+
 
 # Define the filter array
 filters = [
@@ -43,7 +48,28 @@ def process_file(input_file, output_file):
     with open(output_file, 'w') as f:
         pass
         # f.write(filtered_content)
+# HTML content that is to be converted to a PDF
+html_content = """
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>Example with Hyperlinks</h1>
+    <p>Visit <a href="https://www.example.com">Example</a></p>
+</body>
+</html>
+"""
 
+# Create a BytesIO object to store the PDF output
+pdf_output = BytesIO()
+
+# Use xhtml2pdf to convert the HTML content to a PDF and store it in pdf_output
+pisa.CreatePDF(html_content, dest=pdf_output, encoding='utf-8')
+
+# Open a PDF file for writing in binary mode
+with open("html-to-pdf.pdf", "wb") as pdf_file:
+  
+  # Write the PDF content to the file  
+  pdf_file.write(pdf_output.getvalue())
 # Example usage
 input_file = 'style.css'
 output_file = 'style_processed.html'
